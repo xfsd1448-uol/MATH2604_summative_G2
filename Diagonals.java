1e1fd21a851eb803d734bfb3c81b5c86000aa1f9
xfsd1448-uol <xfsd1448@leeds.ac.uk>
2026-04-12 16:34:11 +0100

    Add initial implementation of inverse method for Diagonals

--- Diagonals.java
+++ Diagonals.java
@@ -3,4 +3,23 @@ import static java.lang.Math.*;
 class Diagonals
 {
 
+
+
+
+    public static double[] inverse(double[] a) {
+        if (a == null) return null;
+
+        int n = a.length;
+        double[] inv = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            inv[i] = 1.0 / a[i];
+        }
+
+        return inv;
+    }
+
+
+
+    
 }
