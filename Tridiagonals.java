f536546db75ea7f4aa4b63e2836815a4e22df39b
xfsd1448-uol <xfsd1448@leeds.ac.uk>
2026-04-19 18:22:38 +0100

    Initial implementation of sum method in Tridiagonals

--- Tridiagonals.java
+++ Tridiagonals.java
@@ -7,4 +7,39 @@ static double[][] exampleMatrix(int n)
         double m[][] = new double [1][1]; //dummy code: write your own
         return m; //dummy code: write your own
     }
+
+
+
+
+
+
+
+
+
+
+
+public static double[][] sum(double[][] A, double[][] B) 
+    {
+
+        if (!isValidTridiagonal(A) || !isValidTridiagonal(B)) {
+            return null;
+        }
+        
+        int n = A[1].length;
+        if (B[1].length != n) {
+            return null;
+        }
+        
+        double[][] result = new double[3][n];
+
+        for (int row = 0; row < 3; row++) {
+            for (int i = 0; i < n; i++) {
+                result[row][i] = A[row][i] + B[row][i];
+            }
+        }
+
+        return result;
+    }
+
+    
 }
